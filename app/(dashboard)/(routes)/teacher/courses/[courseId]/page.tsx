085895c437f5929c1f17c7a926815ab44b8ff615
arvind singharpuria <arvind.020606@gmail.com>
2024-09-10 23:14:36 +0530

    course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,11 @@
 import { IconBadge } from "@/components/icon-badge"
 import { db } from "@/lib/db"
+
 import { auth } from "@clerk/nextjs"
 import { LayoutDashboard } from "lucide-react"
 import { redirect } from "next/navigation"
+
+import { TitleForm } from "./_components/title-form"
 
 const CourseIdPage = async ({
     params
@@ -58,6 +61,10 @@ const CourseIdPage = async ({
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
